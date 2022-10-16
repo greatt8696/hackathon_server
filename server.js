1a9ac130f2a7c35c19f258ebd9418730766a6003
greatt8696 <greatt8696@gmail.com>
2022-10-16 23:12:51 +0900

    server

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 require("dotenv").config();
+const { Post } = require("./mongoose/models/Post");
+const { createUid } = require("./util/createRandom");
 
 const express = require("express");
 
@@ -12,6 +14,8 @@ const PORT = "3600";
 
 const { mongoDb, initDb } = require("./mongoose/dbManager");
 
+const { Server } = require("socket.io");
+
 app.use(express.static("public"));
 
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -24,4 +28,129 @@ app.listen(PORT, () => {
   console.log("Running Server");
 });
 
+const io = new Server(3000);
+
+io.on("connection", (socket) => {
+  socket.on("helloclient", (arg) => {
+    console.log("helloclient 받음", arg);
+  });
+  setInterval(() =>
+    socket.emit(
+      "hello",
+      [100000000 * Math.random(), Date(Date.now())],
+      1
+    )
+  );
+
+  setInterval(() =>
+    socket.emit(
+      "hello2",
+      [100000000 * Math.random(), Date(Date.now())],
+      1
+    )
+  );
+
+  setInterval(() =>
+    socket.emit(
+      "hello3",
+      [100000000 * Math.random(), Date(Date.now())],
+      1
+    )
+  );
+
+  setInterval(() =>
+    socket.emit(
+      "hello4",
+      [100000000 * Math.random(), Date(Date.now())],
+      1
+    )
+  );
+  
+  socket.on("joinRoom", ({ joinRoom }) => {
+    socket.join(joinRoom);
+    io.to(joinRoom).emit("helloTestRoom", "helloTestRoom");
+    console.log(joinRoom);
+  });
+});
+
 initDb();
+
+Post.insertMany([
+  {
+    uid: createUid(),
+    title: "뀨뀨뀨",
+    age: parseInt(36 * Math.random()),
+  },
+  {
+    uid: createUid(),
+    title: "뀨뀨뀨",
+    age: parseInt(36 * Math.random()),
+  },
+  {
+    uid: createUid(),
+    title: "뀨뀨뀨",
+    age: parseInt(36 * Math.random()),
+  },
+  {
+    uid: createUid(),
+    title: "뀨뀨뀨",
+    age: parseInt(36 * Math.random()),
+  },
+  {
+    uid: createUid(),
+    title: "뀨뀨뀨",
+    age: parseInt(36 * Math.random()),
+  },
+  {
+    uid: createUid(),
+    title: "뀨뀨뀨",
+    age: parseInt(36 * Math.random()),
+  },
+  {
+    uid: createUid(),
+    title: "뀨뀨뀨",
+    age: parseInt(36 * Math.random()),
+  },
+  {
+    uid: createUid(),
+    title: "뀨뀨뀨",
+    age: parseInt(36 * Math.random()),
+  },
+  {
+    uid: createUid(),
+    title: "뀨뀨뀨",
+    age: parseInt(36 * Math.random()),
+  },
+  {
+    uid: createUid(),
+    title: "뀨뀨뀨",
+    age: parseInt(36 * Math.random()),
+  },
+  {
+    uid: createUid(),
+    title: "뀨뀨뀨",
+    age: parseInt(36 * Math.random()),
+  },
+  {
+    uid: createUid(),
+    title: "뀨뀨뀨",
+    age: parseInt(36 * Math.random()),
+  },
+  {
+    uid: createUid(),
+    title: "뀨뀨뀨",
+    age: parseInt(36 * Math.random()),
+  },
+  {
+    uid: createUid(),
+    title: "뀨뀨뀨",
+    age: parseInt(36 * Math.random()),
+  },
+]);
+
+Post.findById({ uid: "AS3Mxf8D0Gr6nRU19278" });
+
+app.get("/user", async (req, res) => {
+  const user = await Post.find({});
+  res.send(user);
+});
