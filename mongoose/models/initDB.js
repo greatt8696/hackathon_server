1b173f3cf882253270edaa033e5f35f260800181
greatt8696 <88331836+greatt8696@users.noreply.github.com>
2022-10-19 09:37:00 +0900

    socket, db 수정

--- mongoose/models/initDB.js
+++ mongoose/models/initDB.js
@@ -1,15 +1,19 @@
 const {
   Schema,
-  connect,
   mongo: { ObjectId },
 } = require("mongoose");
 const { createObjectId } = require("../../util/createRandom");
-const { Organization, User, Asset, Recycle } = require("./");
+const { Organization, User, GreenFund, TechFund, Recycle } = require("./");
 
 const dummyDatas = require("./dummyData.json");
 
 const asset = dummyDatas.Asset;
 
 connect();
+Organization.deleteAll();
+User.deleteAll();
+GreenFund.deleteAll();
+TechFund.deleteAll();
+Recycle.deleteAll();
 
-module.exports = { initDb, createObjectId };
+module.exports = { createObjectId };
